bffab1f6bdd2aec6d1f0435fd6f46ab2b9eda467
Nael Djanym <djanym@users.noreply.github.com>
2020-05-19 07:26:45 +0300

    Update deploy.sample.php

--- deploy.sample.php
+++ deploy.sample.php
@@ -1,12 +1,12 @@
 <?php
 define("TOKEN", "secret-token");                                       // The secret token to add as a GitHub or GitLab secret, or otherwise as https://www.example.com/?token=secret-token
 define("REMOTE_REPOSITORY", "git@github.com:username/repository.git"); // The SSH URL to your repository
-define("DIR", "/var/www/vhosts/repository/");                          // The path to your repostiroy; this must begin with a forward slash (/)
+define("DIR", "/var/www/html/git-deploy/");                            // The path to your repostiroy; this must begin with a forward slash (/)
 define("BRANCH", "refs/heads/master");                                 // The branch route
-define("LOGFILE", "deploy.log");                                       // The name of the file you want to log to.
+define("LOGFILE", "/var/www/html/git-deploy/deploy.log");              // The name of the file you want to log to.
 define("GIT", "/usr/bin/git");                                         // The path to the git executable
 define("MAX_EXECUTION_TIME", 180);                                     // Override for PHP's max_execution_time (may need set in php.ini)
-define("BEFORE_PULL", "");                                             // A command to execute before pulling
+define("BEFORE_PULL", "git reset --hard ; git clean -f");              // A command to execute before pulling
 define("AFTER_PULL", "");                                              // A command to execute after successfully pulling
 
 require_once("deployer.php");
